5a7682729a611dd2f5bb38b8615432c17a41c845
kss418 <108602943+kss418@users.noreply.github.com>
2026-03-23 18:54:56 +0900

    [Gold II] Title: 공항, Time: 8 ms, Memory: 2972 KB -BaekjoonHub

--- 백준/Gold/10775. 공항/공항.cc
+++ 백준/Gold/10775. 공항/공항.cc
@@ -1,31 +1,51 @@
-#include <iostream>
-#include <set>
-#include <algorithm>
-using namespace std;
-int n, p, num;
-set <int> arr;
+#include <bits/stdc++.h>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long; using u64 = uint64_t;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
 
-int main() {
-    cin >> n >> p;
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 101010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+int p[MAX], sz[MAX];
+bool use[MAX];
 
-    for (int i = 1; i <= n; i++) {
-        arr.insert(-i);
-    }
+int find(int x){
+    if(p[x] == -1) return x;
+    return p[x] = find(p[x]);
+}
+
+void merge(int a, int b){
+    a = find(a); b = find(b);
+    if(a == b) return;
+    p[b] = a;
+}
+
+void run(){
+    cin >> n >> m; memset(p, -1, sizeof(p));
+    for(int i = 1;i <= n;i++) sz[i] = 1;
 
     int result = 0;
-    while (p--) {
-        cin >> num;
-        auto it = arr.lower_bound(-num);
-        if (it == arr.end()) {
-            break;
-        }
+    for(int i = 1;i <= m;i++){
+        int v; cin >> v;
+        int mx = find(v);
+        if(use[mx]) break; use[mx] = 1;
+        if(mx != 1) merge(mx - 1, mx);
         result++;
-        arr.erase(it);
     }
 
     cout << result;
+}
 
-
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
 
     return 0;
 }
