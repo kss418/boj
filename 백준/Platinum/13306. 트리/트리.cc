932f0e5331989989a04ee1cddc37c470274f4cf3
kss418 <108602943+kss418@users.noreply.github.com>
2026-03-23 19:18:36 +0900

    [Platinum IV] Title: 트리, Time: 120 ms, Memory: 27104 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/13306. 트리/트리.cc
+++ 백준/Platinum/13306. 트리/트리.cc
@@ -1,78 +1,67 @@
 #include <bits/stdc++.h>
-#define fastio 	cin.tie(0), cout.tie(0),ios::sync_with_stdio(0);
-using namespace std; typedef long long ll;
-typedef pair<int, int> pi; typedef pair<ll, ll> pll;
-typedef unsigned long long ull;
-ll n, m, k, t; string s;
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long; using u64 = uint64_t;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
 
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 201010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+int p[MAX], sz[MAX], pa[MAX];
 
-const ll INF = 0x3f3f3f3f3f3f3f3f;
-const int MAX = 201010;
-
-struct query{
-  ll num, a, b;
-};
-
-ll p[MAX]; stack <query> st;
-stack <int> result;
-ll uf[MAX];
-
-ll find(ll a){
-    if(uf[a] < 0) return a;
-    return uf[a] = find(uf[a]);
+int find(int x){
+    if(p[x] == -1) return x;
+    return p[x] = find(p[x]);
 }
 
-void merge(ll a, ll b){
+void merge(int a, int b){
     a = find(a); b = find(b);
     if(a == b) return;
-    uf[b] = a;
+    if(sz[a] < sz[b]) swap(a, b);
+    p[b] = a; sz[a] += sz[b];
 }
 
+vector <tuple<int, int, int>> query;
+vector <string> result;
+void run(){
+    cin >> n >> m; memset(p, -1, sizeof(p));
+    for(int i = 2;i <= n;i++) cin >> pa[i];
+    for(int i = 1;i <= n;i++) sz[i] = 1;
+
+    int q = n + m - 1;
+    while(q--){
+        int op; cin >> op;
+        if(!op){
+            int x; cin >> x;
+            query.push_back({op, x, pa[x]});
+        }
+        else{
+            int a, b; cin >> a >> b;
+            query.push_back({op, a, b});
+        }
+    }
+
+    reverse(all(query));
+    for(auto& [op, a, b] : query){
+        if(!op) merge(a, b);
+        else{
+            result.push_back(find(a) == find(b) ? "YES" : "NO");
+        }
+    }
+
+    reverse(all(result));
+    for(auto& i : result) cout << i << "\n";
+}
 
 int main() {
-	fastio;
-	
-	memset(uf,-1,sizeof(uf));
-	cin >> n >> m;
-	for(int i = 2; i <= n;i++){
-	    cin >> p[i];
-	}
-	
-	ll ag = m + n - 1;
-	while(ag--){
-	    ll num,a,b;
-	    b = 0;
-	    cin >> num;
-	    
-	    if(!num){
-	        cin >> a;
-	    }
-	    else{
-	        cin >> a >> b;
-	    }
-	    
-	    query cur = {num,a,b};
-	    st.push(cur);
-	}
-	
-	while(!st.empty()){
-	    query cur = st.top();
-	    st.pop();
-	    if(cur.num){
-	        result.push(find(cur.a) == find(cur.b));
-	    }
-	    else{
-	        merge(cur.a,p[cur.a]);
-	    }
-	}
-	
-	while(!result.empty()){
-	    if(result.top() == 1) cout << "YES\n";
-	    else cout << "NO\n";
-	    result.pop();
-	}
+    fastio; // cin >> t;
+    while(t--) run(); 
 
-   
-    
-	return 0;
+    return 0;
 }
