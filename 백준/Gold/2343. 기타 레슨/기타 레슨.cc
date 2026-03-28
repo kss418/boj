404c5d56133d8c4d9ffee52d2e693318b72f4c7c
kss418 <108602943+kss418@users.noreply.github.com>
2026-03-29 03:44:31 +0900

    [Gold V] Title: 기타 레슨, Time: 8 ms, Memory: 2476 KB -BaekjoonHub

---
+++ 백준/Gold/2343. 기타 레슨/기타 레슨.cc
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+#define all(x) (x).begin(), (x).end()
+#define x first 
+#define y second
+using namespace std; using ll = long long; using u64 = uint64_t;
+using ld = long double; using pld = pair<ld, ld>;
+using i128 = __int128_t; using f128 = __float128; 
+using pll = pair<ll, ll>; using tll = tuple<ll, ll, ll>;
+ll n, m, k, t = 1; string s;
+
+constexpr ll INF = 0x3f3f3f3f3f3f3f3f;
+constexpr ll MINF = 0xc0c0c0c0c0c0c0c0;
+constexpr ll MAX = 101010; // SET MAX SIZE
+constexpr ll MOD = 998244353;
+
+class _bs { // integer
+public:
+    template <class F>
+    static ll max_right(ll l, ll r, const F& f) {
+        ll lo = l, hi = r, mid = -1;
+        while (hi > lo) {
+            mid = (hi + lo + 1) >> 1ll;
+            if (f(mid)) lo = mid;
+            else hi = mid - 1;
+        }
+        return lo;
+    }
+
+    template <class F>
+    static ll min_left(ll l, ll r, const F& f) {
+        ll lo = l, hi = r, mid = -1;
+        while (hi > lo) {
+            mid = (hi + lo) >> 1ll;
+            if (f(mid)) hi = mid;
+            else lo = mid + 1;
+        }
+        return lo;
+    }
+};
+
+int a[MAX];
+bool f(ll cur){
+    int cnt = 1; ll sum = 0;
+    for(int i = 1;i <= n;i++){
+        if(a[i] > cur) return 0;
+        if(sum + a[i] > cur) sum = 0, cnt++;
+        sum += a[i];
+    }
+
+    return cnt <= m;
+}
+
+void run(){
+    cin >> n >> m; 
+    for(int i = 1;i <= n;i++) cin >> a[i];
+    cout << _bs::min_left(0, 1e9, f);
+}
+
+int main() {
+    fastio; // cin >> t;
+    while(t--) run(); 
+
+    return 0;
+}
